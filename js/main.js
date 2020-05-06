d77475b506c4f076caddaa42acd1d5c16878548b
hoshi <falloftroy2000@yahoo.co.jp>
2020-05-06 12:47:35 +0900

    スマホメニュー2

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
 $(function () {
- const $win = $(window);
- const $btnMenu = $('.btn-menu');
- const $nav = $('header > nav');
- const animate = 'is-animate';
- const isOpen = 'is-open';
+  const $win = $(window);
+  const $btnMenu = $('.btn-menu');
+  const $nav = $('header > nav');
+  const isAnimate = 'is-animate';
+  const isOpen = 'is-open';
 
- $btnMenu.on('click', function () {
+  // ボタンクリック時の処理
+  $btnMenu.on('click', function () {
+    const $this = $(this);
+    if ($nav.is(':animated')) {
+      return;
+    }
+    if (!$this.hasClass(isOpen)) {
+      $nav.slideDown();
+      $this.addClass(isOpen).addClass(isAnimate);
+    } else {
+      $nav.slideUp();
+      $this.removeClass(isOpen).removeClass(isAnimate);
+    }
+  });
 
-    //ボタンをクリックしたときの記述
+  // ウィンドウリサイズ時の処理
+  $win.on('resize', function () {
+    if ($win.width() > 768) {
+      $nav.show();
+      $btnMenu.removeClass(isOpen).removeClass(isAnimate);
+    } else {
+      if (!$btnMenu.hasClass(isOpen)) {
+        $nav.hide();
+      }
+    }
+  });
 
- });
-
- function resizeDisplay() {
-
-    //ウィンドウ幅を変えた時の記述
-
- }
- resizeDisplay();
-
- $win.on('resize', function () {
-   resizeDisplay();
- });
 });
